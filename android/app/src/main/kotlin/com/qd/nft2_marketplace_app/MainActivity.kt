ed84ae1735f7c6fb7979ce72b1eedaa97e9b17c0
QD1221 <zxcvbnm12122000@gmail.com>
2021-10-08 07:39:57 +0700

    Initial commit

---
+++ android/app/src/main/kotlin/com/qd/nft2_marketplace_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.qd.nft2_marketplace_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/ui/home_page.dart
@@ -0,0 +1,148 @@
+import 'package:flutter/material.dart';
+import 'package:nft2_marketplace_app/ui/explore_page.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _pageIndex = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: IndexedStack(
+          index: _pageIndex,
+          children: [
+            ExplorePage(),
+            Container(
+              color: Colors.blue,
+              child: Center(
+                child: Text('Search'),
+              ),
+            ),
+            Container(
+              color: Colors.red,
+              child: Center(
+                child: Text('Favorite'),
+              ),
+            ),
+            Container(
+              color: Colors.brown,
+              child: Center(
+                child: Text('Wallet'),
+              ),
+            ),
+          ],
+        ),
+      ),
+      bottomNavigationBar: BottomAppBar(
+        child: SizedBox(
+          height: 64,
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              children: [
+                Expanded(
+                  child: GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        _pageIndex = 0;
+                      });
+                    },
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(
+                          Icons.explore,
+                          color: _pageIndex == 0 ? Colors.black : Colors.grey,
+                        ),
+                        Text(
+                          'Explore',
+                          style: TextStyle(
+                            color: _pageIndex == 0 ? Colors.black : Colors.grey,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        _pageIndex = 1;
+                      });
+                    },
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(
+                          Icons.search,
+                          color: _pageIndex == 1 ? Colors.black : Colors.grey,
+                        ),
+                        Text(
+                          'Search',
+                          style: TextStyle(
+                            color: _pageIndex == 1 ? Colors.black : Colors.grey,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        _pageIndex = 2;
+                      });
+                    },
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(
+                          Icons.favorite_border,
+                          color: _pageIndex == 2 ? Colors.black : Colors.grey,
+                        ),
+                        Text('Favorite',
+                          style: TextStyle(
+                            color: _pageIndex == 2 ? Colors.black : Colors.grey,
+                          ),),
+                      ],
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        _pageIndex = 3;
+                      });
+                    },
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(
+                          Icons.account_balance_wallet_outlined,
+                          color: _pageIndex == 3 ? Colors.black : Colors.grey,
+                        ),
+                        Text('Explore',
+                          style: TextStyle(
+                            color: _pageIndex == 3 ? Colors.black : Colors.grey,
+                          ),),
+                      ],
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
